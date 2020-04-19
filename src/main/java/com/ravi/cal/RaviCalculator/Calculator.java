46375343df3649bb28c4d652104ab9923442e726
ravi2krishna <ravi2krishna@gmail.com>
2020-04-19 22:55:47 +0530

    Calculator Completed

---
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -0,0 +1,49 @@
+package com.ravi.cal.RaviCalculator;
+
+public class Calculator {
+
+	public long first;
+	public long second;
+	
+	public Calculator(long first, long second){
+		this.first = first;
+		this.second=second;
+	}
+	
+	public long getFirst() {
+		return first;
+	}
+
+	public long getSecond() {
+		return second;
+	}
+
+	public long addFucn(long first, long second){
+		
+		return first+second;
+	}
+	
+	public long subFucn(long first, long second){
+		
+		return second-first;
+	}
+	
+	public long mulFucn(long first, long second){
+		
+		return first/second;
+	}
+	
+	
+	public static void main(String[] args) {
+
+		long first = Long.parseLong(args[0]);
+		long second = Long.parseLong(args[1]);
+		
+		Calculator cal = new Calculator(first, second);
+		
+		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second));
+	    System.out.println("CALCULATOR DISPLAY");
+	    System.out.println(output);
+	}
+	
+}
