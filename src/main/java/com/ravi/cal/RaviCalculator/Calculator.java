7f9684b9638941d8788894534e1965e13abb7b3e
ravi2krishna <ravi2krishna@gmail.com>
2020-09-22 12:22:42 +0530

    webhook test

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -43,7 +43,7 @@ public class Calculator {
 		
 		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second));
 	    	System.out.println(output);
-	    	System.out.println("New Code Updated");
+	    	System.out.println("Test WebHook");
 
 	}
 	
